8651a7a6f54e9ee8e6fbee7876d2539229ae855c
Niels Bækgaard-Sørensen <nbs@baekgaardit.dk>
2018-04-25 21:07:45 +0200

    Created intellij project
    Created intellij project and added some logging libraries.

---
+++ src/top.java
@@ -0,0 +1,15 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class top {
+
+    private static Logger logger = LogManager.getLogger();
+
+    public static void main (String[] argsv){
+
+        logger.trace("Test");
+
+
+
+    }
+}
